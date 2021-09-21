a167c30749ff901b10947348785ec483d51a3093
anggi-a11y <ahkmhp@gmail.com>
2021-09-21 12:11:32 +0700

    menambahkan file contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Anggi Hikmah putri
+		     saya tinggal di daerah bogor
+		     olahraga yang saya sukai adalah Berenang dan Bulutangkis";
+	}
+}
